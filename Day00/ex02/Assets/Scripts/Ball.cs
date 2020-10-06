780e376b05b6b2aa1b5dd757a34fa146fb86708e
dolovnyak <dolovnyak@yandex.ru>
2020-10-06 15:14:45 +0300

    ex02 done

---
+++ Day00/ex02/Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] private GameObject hole;
+    public bool IsMoving { get; private set; }
+    public bool IsDestroyed { get; private set; }
+    private float movement;
+    private int dir;
+    private int score;
+    float Abs(float a) => a >= 0 ? a : -a;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsMoving = false;
+        IsDestroyed = false;
+        movement = 0.0f;
+        dir = 1;
+        score = -20;
+    }
+
+    public void GotHit(float punchPower, int dir)
+    {
+        IsMoving = true;
+        movement = punchPower * 100;
+        this.dir = dir;
+        score += 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movement > 0.0f)
+        {
+            if (gameObject.transform.localPosition.y >= 4.5f)
+            {
+                dir = -1;
+            }
+            else if (gameObject.transform.localPosition.y <= -4.5f)
+            {
+                dir = 1;
+            }
+            movement -= 0.1f;
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y + movement * dir * Time.deltaTime, gameObject.transform.localPosition.z);
+        }
+        else
+        {
+            if (Abs(gameObject.transform.localPosition.y - hole.transform.localPosition.y) < 0.4)
+            {
+                Destroy(gameObject);
+                IsDestroyed = true;
+                Debug.Log("Score: " + score);
+            }
+            IsMoving = false;
+        }
+    }
+}
---
+++ Day00/ex02/Assets/Scripts/Club.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Club : MonoBehaviour
+{
+    [SerializeField] private GameObject ballGameObject;
+    [SerializeField] private GameObject hole;
+    private Ball ball;
+    private bool isSwing;
+    private float punchPower;
+    private bool previousIsBallMoving;
+    private float previousClubPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = ballGameObject.GetComponent<Ball>();
+        isSwing = false;
+        punchPower = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ball.IsDestroyed)
+        {
+            if (!ball.IsMoving)
+            {
+                if (Input.GetKeyDown("space"))
+                {
+                    isSwing = true;
+                }
+                else if (Input.GetKeyUp("space"))
+                {
+                    isSwing = false;
+                    gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, ball.transform.localPosition.y, gameObject.transform.localPosition.z);
+                    if (ball.transform.localPosition.y > hole.transform.localPosition.y)
+                    {
+                        ball.GotHit(punchPower, -1);
+                    }
+                    else
+                    {
+                        ball.GotHit(punchPower, 1);
+                    }
+                    punchPower = 0.0f;
+                }
+                if (previousIsBallMoving)
+                {
+                    gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, ball.transform.localPosition.y, gameObject.transform.localPosition.z);
+                    if (gameObject.transform.localPosition.y < hole.transform.localPosition.y)
+                    {
+                        gameObject.transform.localRotation = new Quaternion(0.0f, 0.0f, 0.0f, 0.0f);
+                    }
+                    else
+                    {
+                        gameObject.transform.localRotation = new Quaternion(1.0f, 0.0f, 0.0f, 0.0f);
+                    }
+                }
+            }
+            if (isSwing)
+            {
+                if (punchPower < 1.0f)
+                    punchPower += 0.002f;
+                if (gameObject.transform.localPosition.y < hole.transform.localPosition.y)
+                    gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y - 0.01f, gameObject.transform.localPosition.z);
+                if (gameObject.transform.localPosition.y > hole.transform.localPosition.y)
+                    gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y + 0.01f, gameObject.transform.localPosition.z);
+            }
+
+            previousIsBallMoving = ball.IsMoving;
+        }
+    }
+}
