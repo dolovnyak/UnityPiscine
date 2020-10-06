3453a716a35c8714ea6c49b80d62d982637ec885
dolovnyak <dolovnyak@yandex.ru>
2020-10-06 19:01:28 +0300

    ex03 done

---
+++ Day00/ex03/Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public bool IsGameOver { get; set; }
+    public int Score { get; set; }
+    [SerializeField] private float gravitation;
+    [SerializeField] private float jumpEnergy;
+    private float movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsGameOver = false;
+        movement = 0.0f;
+        Score = 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Score: " + Score);
+        Debug.Log("Time: " + Mathf.RoundToInt(Time.time) + "s");
+        IsGameOver = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsGameOver)
+        {
+            return;
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            movement = jumpEnergy;
+        }
+        movement -= gravitation;
+        gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y + movement * Time.deltaTime, gameObject.transform.localPosition.z);
+    }
+}
---
+++ Day00/ex03/Assets/Scripts/Pipe.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    [SerializeField] private GameObject playerGameObject;
+    [SerializeField] private GameObject pipe1;
+    [SerializeField] private GameObject pipe2;
+    private Bird player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = playerGameObject.GetComponent<Bird>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.IsGameOver)
+            return;
+        if (pipe1.transform.localPosition.x < -6.0f)
+        {
+            pipe1.transform.localPosition = new Vector3(9.0f, pipe1.transform.localPosition.y, pipe1.transform.localPosition.z);
+            player.Score += 1;
+        }
+        if (pipe2.transform.localPosition.x < -6.0f)
+        {
+            pipe2.transform.localPosition = new Vector3(9.0f, pipe2.transform.localPosition.y, pipe2.transform.localPosition.z);
+            player.Score += 1;
+        }
+        pipe1.transform.localPosition = new Vector3(pipe1.transform.localPosition.x - 0.01f, pipe1.transform.localPosition.y, pipe1.transform.localPosition.z);
+        pipe2.transform.localPosition = new Vector3(pipe2.transform.localPosition.x - 0.01f, pipe2.transform.localPosition.y, pipe2.transform.localPosition.z);
+    }
+}
