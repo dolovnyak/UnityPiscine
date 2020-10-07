0f42e2d7e3b7b5d262ff283dffa03f9792853e41
dolovnyak <dolovnyak@yandex.ru>
2020-10-08 00:36:59 +0300

    Day01 ex00 done

---
+++ Day01/Assets/ex00/Scripts/PlayerScript_ex00.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript_ex00 : MonoBehaviour
+{
+    public bool IsActivePlayer { get; set; }
+    [SerializeField] LayerMask layers;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float movementSpeed;
+    private KeyCode _keyLeft1;
+    private KeyCode _keyRight1;
+    private KeyCode _keyLeft2;
+    private KeyCode _keyRight2;
+    private Rigidbody2D _rb;
+    private Transform _circleTransform;
+    private float _circleRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _keyLeft1 = KeyCode.A;
+        _keyRight1 = KeyCode.D;
+        _keyLeft2 = KeyCode.LeftArrow;
+        _keyRight2 = KeyCode.RightArrow;
+        _rb = this.GetComponent<Rigidbody2D>();
+        _circleTransform = gameObject.transform.GetChild(0);
+        _circleRadius = this.GetComponent<Collider2D>().bounds.size.x / 2.0f - 0.01f;
+        Debug.Log(_circleRadius);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsActivePlayer)
+        {
+            return;
+        }
+
+        var velocity = _rb.velocity;
+        if (Input.GetKey(_keyLeft1) || Input.GetKey(_keyLeft2))
+        {
+            velocity.x = -2.5f;
+        }
+        else if (Input.GetKey(_keyRight1) || Input.GetKey(_keyRight2))
+        {
+            velocity.x = 2.5f;
+        }
+        else if (Input.GetKeyUp(_keyLeft1) || Input.GetKeyUp(_keyLeft2))
+        {
+            velocity.x = 0.0f;
+        }
+        else if (Input.GetKeyUp(_keyRight1) || Input.GetKeyUp(_keyRight2))
+        {
+            velocity.x = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsCanJump())
+        {
+            velocity.y = jumpSpeed;
+        }
+
+        _rb.velocity = velocity;
+    }
+
+    bool IsCanJump()
+    {
+        var hit = Physics2D.CircleCast((Vector2)_circleTransform.position, _circleRadius, Vector2.down, 0.0f, layers);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Day01/Assets/ex00/Scripts/PlayersController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersController : MonoBehaviour
+{
+    [SerializeField] private GameObject john;
+    [SerializeField] private GameObject thomas;
+    [SerializeField] private GameObject claire;
+    private PlayerScript_ex00 _john;
+    private PlayerScript_ex00 _thomas;
+    private PlayerScript_ex00 _claire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _john = john.GetComponent<PlayerScript_ex00>();
+        _thomas = thomas.GetComponent<PlayerScript_ex00>();
+        _claire = claire.GetComponent<PlayerScript_ex00>();
+        _john.IsActivePlayer = true;
+        _thomas.IsActivePlayer = false;
+        _claire.IsActivePlayer = false;
+        john.GetComponent<Rigidbody2D>().isKinematic = false;
+        claire.GetComponent<Rigidbody2D>().simulated = false;
+        thomas.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            _john.IsActivePlayer = true;
+            _thomas.IsActivePlayer = false;
+            _claire.IsActivePlayer = false;
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            _john.IsActivePlayer = false;
+            _thomas.IsActivePlayer = true;
+            _claire.IsActivePlayer = false;
+        }
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            _john.IsActivePlayer = false;
+            _thomas.IsActivePlayer = false;
+            _claire.IsActivePlayer = true;
+        }
+    }
+}
