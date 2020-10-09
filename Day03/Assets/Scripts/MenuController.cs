83a9d0dff366430101e0a0966fda15688e86b5a4
dolovnyak <dolovnyak@yandex.ru>
2020-10-09 20:24:37 +0300

    ex00 done

---
+++ Day03/Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        Application.LoadLevel("Level00");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
