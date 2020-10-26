b4689828ac299aa50570471c040740a4d85e7c1d
dolovnyak <dolovnyak@yandex.ru>
2020-10-26 23:25:10 +0300

    added camera which interactive with canon

--- Day07/Assets/Scripts/PlayerController.cs
+++ Day07/Assets/Scripts/PlayerController.cs
@@ -7,49 +7,57 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject canon = null;
     [SerializeField] private float speed = 0;
-    [SerializeField] private Rigidbody rbLeft = null;
-    [SerializeField] private Rigidbody rbRight = null;
-    [SerializeField] private NavMeshAgent agent = null;
-    [SerializeField] private Transform pizdecForward = null;
+    [SerializeField] private LayerMask solidSurfaceLayers = 0;
+    private bool _isOnSolidSurface;
     private float gravity = 200f;
     private Rigidbody _rb;
+    private float _deltaYCanon;
 
-    // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        _rb.centerOfMass = new Vector3(0f, 0f, 0f);
-        // _rb.AddForce(Vector3.down * 1000);
-        // rbLeft.AddForce(Vector3.down * gravity);
-        // rbRight.AddForce(Vector3.down * gravity);
-        // rbRight.centerOfMass = new Vector3(1f, 0f, 0.5f);
+        _rb.centerOfMass = new Vector3(0f, -0.5f, 0f);
+        _deltaYCanon = Mathf.Abs(transform.position.y - canon.transform.position.y);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // rbLeft.centerOfMass = new Vector3(-1f, 0f, 0.5f);
-        // rbRight.centerOfMass = new Vector3(1f, 0f, 0.5f);
         // Debug.Log("rb: " + _rb.centerOfMass);
-        // Debug.Log("rbl: " + rbLeft.centerOfMass);
-        // Debug.Log("rbr: " + rbRight.centerOfMass);
-        // Debug.Log(_rb.)
+        // Debug.Log(transform.rotation);
+
+        // if (_isOnSolidSurface)
+        // {
+        //     Movement();
+        // }
+        if (IsOnSolidSurface());
+        {
+            Movement();
+        }
+        Rotation();
+
+        canon.transform.position = transform.position + transform.up * _deltaYCanon;
+    }
+
+    void Rotation()
+    {
+        var rotateVelocity = new Vector3(0f, 100f, 0f);
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(-rotateVelocity * Time.deltaTime);
+            _rb.MoveRotation(_rb.rotation * deltaRotation);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion deltaRotation = Quaternion.Euler(rotateVelocity * Time.deltaTime);
+            _rb.MoveRotation(_rb.rotation * deltaRotation);
+        }
+    }
+
+    void Movement()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            // rbLeft.velocity = transform.forward * speed;
-            // rbRight.velocity = transform.forward * speed;
-
-            // _rb.AddForce(transform.forward * speed);
-            // rbLeft.AddForce(transform.forward * speed);
-            // rbRight.AddForce(transform.forward * speed);
-
             _rb.AddForce(transform.forward * speed);
-            // rbLeft.AddForce(transform.forward * speed);
-            // rbRight.AddForce(transform.forward * speed);
-
-            // rbLeft.velocity = Vector3.Normalize(transform.forward) * speed;
-            // rbRight.velocity = Vector3.Normalize(transform.forward) * speed;
-            // _rb.velocity = Vector3.Normalize(transform.forward) * speed;
             // _rb.velocity = transform.forward * speed;
             // RaycastHit hit;
             // if (Physics.Raycast(pizdecForward.position, -pizdecForward.up, out hit))
@@ -61,25 +69,76 @@ public class PlayerController : MonoBehaviour
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            // _rb.velocity = Vector3.Normalize(transform.forward * -speed);
-            // rbLeft.velocity = transform.forward * -speed;
+            _rb.AddForce(transform.forward * -speed);
         }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            // rbLeft.velocity = Vector3.zero;
-        }
-        // rbLeft.velocity = Vector3.Normalize(rbLeft.velocity + Vector3.down * gravity);
-        // rbRight.velocity = Vector3.Normalize(rbRight.velocity + Vector3.down * gravity);
+    }
 
-        if (Input.GetKey(KeyCode.A))
+    private bool IsOnSolidSurface()
+    {
+        // Gizmos.color = Color.red;
+        var boxSize = new Vector3(2f, 1f, 2f);
+        RaycastHit[] hits = Physics.BoxCastAll(transform.position + transform.up * 3f, boxSize, -transform.up, transform.rotation, 2f, solidSurfaceLayers);
+        if (hits != null && hits.Length != 0)
         {
-            transform.forward = Quaternion.AngleAxis(-1, transform.up) * transform.forward;
+            Debug.Log("AAAA");
+            Debug.DrawLine(transform.position + transform.up * 3f, hits[0].point, Color.red);
+            return true;
+            // Gizmos.DrawRay(transform.position, -transform.up * hit.distance);
+            // Gizmos.DrawWireCube(transform.position - transform.up * hit.distance, transform.localScale);
         }
-        if (Input.GetKey(KeyCode.D))
+        return false;
+    }
+    
+    // void OnTriggerEnter(Collider other)
+    // {
+    //     Debug.Log("AA");
+    //     _isOnSolidSurface = true;
+    //     // if (IsInLayerMask(playerLayers, other.gameObject.layer))
+    //     // {
+    //     // }
+    // }
+
+    // void OnTriggerExit(Collider other)
+    // {
+    //     Debug.Log("BB");
+    //     _isOnSolidSurface = false;
+    //     // if (IsInLayerMask(playerLayers, other.gameObject.layer))
+    //     // {
+    //     // }
+    // }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (IsInLayerMask(solidSurfaceLayers, other.gameObject.layer))
         {
-            transform.forward = Quaternion.AngleAxis(1, transform.up) * transform.forward;
+            Debug.Log("OO");
+            _isOnSolidSurface = true;
         }
-        canon.transform.position = new Vector3(canon.transform.position.x, transform.position.y, transform.position.z);
-        // _rb.AddForce(Physics.gravity * _rb.mass);
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (IsInLayerMask(solidSurfaceLayers, other.gameObject.layer))
+        {
+            Debug.Log("YY");
+            _isOnSolidSurface = false;
+        }
+    }
+
+    bool IsInLayerMask(LayerMask mask, int layer)
+    {
+        return mask == (mask | 1 << layer);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //Check if there has been a hit yet
+            //Draw a Ray forward from GameObject toward the hit
+        //  Gizmos.DrawRay(transform.position, transform.forward * m_Hit.distance);
+            //Draw a cube that extends to where the hit exists
+        Gizmos.DrawWireCube(transform.position + transform.up, new Vector3(transform.localScale.x * 3f, transform.localScale.y, transform.localScale.z * 3f));
     }
 }
